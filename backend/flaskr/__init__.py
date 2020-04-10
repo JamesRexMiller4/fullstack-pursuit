2da02811fae36279c6ddff4a8405e03fe43ff115
James Miller <james.rex.miller.4@gmail.com>
2020-04-10 14:53:04 -0600

    Update import logic and correct spacing mistakes

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,21 +3,20 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
-
-from models import setup_db, Question, Category
+from models import *
 
 QUESTIONS_PER_PAGE = 10
 
 def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
-  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+  CORS(app)
+  # cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.after_request
-def after_request(res):
-  res.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-  res.headers.add('Access-Control-Allow-Methods', 'GET, DELETE, POST')
-  return res
-
+  @app.after_request
+  def after_request(res):
+    res.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    res.headers.add('Access-Control-Allow-Methods', 'GET, DELETE, POST')
+    return res
 
   return app
