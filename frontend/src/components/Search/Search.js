47b3f8df3e2a0363d85f288046cb47bbfec46ceb
James Miller <james.rex.miller.4@gmail.com>
2020-04-10 02:33:48 -0600

    Add Search component logic and html

---
+++ frontend/src/components/Search/Search.js
@@ -0,0 +1,29 @@
+import React, { useState, useRef } from 'react';
+import './Search';
+
+const Search = ({ submitSearch }) => {
+  const [ search, setSearch ] = useState({
+    query: ''
+  });
+  const inputEl = useRef(null);
+
+  const getInfo = (e) => {
+    submitSearch(search.query)
+  }
+
+  const handleInputChange = () => {
+    setSearch({ query: inputEl})
+  }
+
+  return ( 
+    <form onSubmit={getInfo}>
+      <input 
+        placeholder="Search questions..."
+        ref={inputEl}
+        onChange={handleInputChange}/>
+      <input type="submit" value='Submit' className='button' />
+    </form>
+  );
+}
+
+export default Search;
