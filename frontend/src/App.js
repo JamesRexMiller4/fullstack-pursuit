cc5c1494f129ed58821b009c08119803d7ba1a6b
James Miller <james.rex.miller.4@gmail.com>
2020-04-10 02:42:22 -0600

    Add Router, Switch statement and import components to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-function App() {
+import './App.css';
+import FormView from './components/FormView/FormView';
+import QuestionView from './components/QuestionView/QuestionView';
+import Header from './components/Header/Header';
+import QuizView from './components/QuizView/QuizView';
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header path />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={QuestionView} />
+          <Route path="/add" component={FormView} />
+          <Route path="/play" component={QuizView} />
+          <Route component={QuestionView} />
+        </Switch>
+      </Router>
     </div>
   );
 }
