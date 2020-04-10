f559a89079e656ea3bb74fc08e7b766897776807
James Miller <james.rex.miller.4@gmail.com>
2020-04-10 09:59:58 -0600

    Add and configure CORS

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import random
 
 from models import setup_db, Question, Category
@@ -10,6 +11,13 @@ QUESTIONS_PER_PAGE = 10
 def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
+  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.after_request
+def after_request(res):
+  res.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+  res.headers.add('Access-Control-Allow-Methods', 'GET, DELETE, POST')
+  return res
 
 
   return app
