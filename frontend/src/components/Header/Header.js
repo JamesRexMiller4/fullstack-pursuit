5b7603d6855bce7bc6b00a5ea5f18aee1ebcb8e6
James Miller <james.rex.miller.4@gmail.com>
2020-04-10 01:13:24 -0600

    Add logic and html for Header component

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React, {} from 'react';
+import './Header.css';
+
+const Header = () => {
+  const navTo = (uri) => {
+    window.location.href = window.location.origin + uri;
+  }
+
+  return ( 
+    <div className='App-header'>
+      <h1 onClick={navTo('')}>FullStack-Trivia</h1>
+      <h2 onClick={navTo('')}>List</h2>
+      <h2 onClick={navTo('/add')}>Add</h2>
+      <h2 onClick={navTo('/play')}>Play</h2>
+    </div>
+  );
+}
+
+export default Header;
